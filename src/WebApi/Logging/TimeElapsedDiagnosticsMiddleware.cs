7b5220585a85fc7fbd6a3b42ea18de792e0b7b67
robertstriz <robert.striz@3pillarglobal.com>
2022-03-23 15:31:15 +0100

    add app insight BE

---
+++ src/WebApi/Logging/TimeElapsedDiagnosticsMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+using Serilog.Context;
+
+namespace Logging;
+
+public class TimeElapsedDiagnosticsMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public TimeElapsedDiagnosticsMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        Stopwatch sw = Stopwatch.StartNew();
+        try
+        {
+            await _next(context);
+            PushPropertyElapsed(sw);
+            Log.Debug("DiagnosticsMiddleware:OK");
+        }
+        catch (Exception e)
+        {
+            PushPropertyElapsed(sw);
+            Log.Fatal("DiagnosticsMiddleware:ERROR Message: {message} , Exception: {exception}", e.Message,e);
+        }
+    }
+
+    private static void PushPropertyElapsed(Stopwatch sw)
+    {
+        sw.Stop();
+        LogContext.PushProperty("Elapsed", sw.ElapsedMilliseconds);
+    }
+}
