239570d0883eea2e4512dd03cd6ffc57be2fecb6
TerezaSkutova <TerezaSkutova@users.noreply.github.com>
2022-03-24 11:34:04 +0100

    View of QuestionSet

---
+++ src/WebApi/Domain/Models/Views/QuestionSetView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Views
+{
+    public class QuestionSetView
+    {
+        public string Title { get; set; }
+
+        public double? Difficulty { get; set; }
+
+        public IEnumerable<string> Categories { get; set; }
+    }
+}
