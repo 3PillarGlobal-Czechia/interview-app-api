7f3bb887eba93771c0c916602440de30b5300c0e
robertstriz <100909070+robertstriz@users.noreply.github.com>
2022-03-24 12:38:58 +0100

    fix app insight (#62)

---
+++ src/WebApi/WebApi/Modules/ConfigurationExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog;
+
+namespace WebApi.Modules;
+
+public static class ConfigurationExtension
+{
+    public static T GetConfigurationValue<T>(string key , T defaultValue)
+    {
+        Log.Information("GetConfigurationValue => Getting value for {Key}",key);
+       
+        string value = Environment.GetEnvironmentVariable(key);
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            Log.Information("GetConfigurationValue => Getting default value for {Key} => {Value}",key,defaultValue);
+            return defaultValue;
+        }
+
+        T res = (T) Convert.ChangeType(value, typeof(T));
+        Log.Information("GetConfigurationValue => Getting default value for {Key} => {Value}",key,res);
+        return res;
+    }
+}
