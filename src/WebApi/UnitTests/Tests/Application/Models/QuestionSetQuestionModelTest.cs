ac4efe0700c94e92ed01e05dd9a1df3d359b7f10
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-03-28 11:44:11 +0200

    add dummy test for coverage

---
+++ src/WebApi/UnitTests/Tests/Application/Models/QuestionSetQuestionModelTest.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using Xunit;
+
+namespace UnitTests.Tests.Application.Models;
+
+public class QuestionSetQuestionModelTest
+{
+    [Fact]
+    public void QuestionSetQuestionModel_UseProperties()
+    {
+        var model = new QuestionSetQuestionModel
+        {
+            InterviewQuestionId = 0,
+            QuestionListId = 0,
+            QuestionList = null,
+            InterviewQuestion = null,
+            Order = 0
+        };
+
+        Assert.NotNull(model);
+    }
+}
