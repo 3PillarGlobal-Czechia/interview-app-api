14bdb8b92f6d15230459c09db0c63296dcb41e07
TerezaSkutova <TerezaSkutova@users.noreply.github.com>
2022-03-31 19:50:55 +0200

    Unit test fix RemoveQuestionsFromList()

--- src/WebApi/UnitTests/Tests/Application/UseCase/QuestionSetTests/DeleteQuestionSetUseCaseTest.cs
+++ src/WebApi/UnitTests/Tests/Application/UseCase/QuestionSetTests/DeleteQuestionSetUseCaseTest.cs
@@ -42,7 +42,7 @@ namespace UnitTests.Tests.Application.UseCase.QuestionSetTests
             var repositoryMock = new Mock<IQuestionSetRepository>();
             repositoryMock.Setup(x => x.GetById(1).Result).Returns(new QuestionSetModel());
             repositoryMock.Setup(x => x.Delete(1).Result).Returns(true);
-            repositoryMock.Setup(x => x.RemoveQuestionsFromList(It.IsAny<QuestionSetModel>(), It.IsAny<IEnumerable<int>>()).Result).Returns(true);
+            repositoryMock.Setup(x => x.RemoveQuestionsFromList(1, It.IsAny<IEnumerable<int>>()).Result).Returns(true);
             
             var outputPortMock = new Mock<IOutputPort>();
             var useCase = new DeleteQuestionSetUseCase(repositoryMock.Object);
