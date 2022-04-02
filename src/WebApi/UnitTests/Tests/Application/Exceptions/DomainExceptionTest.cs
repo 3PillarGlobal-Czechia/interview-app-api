6a3809eb160b17cccf5f6ae40ccc1d2920604cc4
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-02 19:58:19 +0200

    add test for domain exception

---
+++ src/WebApi/UnitTests/Tests/Application/Exceptions/DomainExceptionTest.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Exceptions;
+using System;
+using System.Runtime.Serialization;
+using Xunit;
+
+namespace UnitTests.Tests.Application.Exceptions;    
+
+public class DomainExceptionTest
+{
+    private sealed class InstantiableDomainException : DomainException
+    {
+        public InstantiableDomainException() { }
+
+        public InstantiableDomainException(string message) : base(message) { }
+
+        public InstantiableDomainException(string message, Exception inner) : base(message, inner) { }
+
+        public InstantiableDomainException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Fact]
+    public void DomainException_CanBeThrown()
+    {
+        static void Throw() => throw new InstantiableDomainException("Test message");
+
+        Assert.ThrowsAny<DomainException>(Throw);
+    }
+}
