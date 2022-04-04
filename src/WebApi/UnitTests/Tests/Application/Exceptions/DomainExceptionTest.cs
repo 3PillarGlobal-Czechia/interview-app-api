63b630c83d31a61884e8889899b659a33237d8fb
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-04 08:15:59 +0200

    remove test for last constructor

--- src/WebApi/UnitTests/Tests/Application/Exceptions/DomainExceptionTest.cs
+++ src/WebApi/UnitTests/Tests/Application/Exceptions/DomainExceptionTest.cs
@@ -14,8 +14,6 @@ public class DomainExceptionTest
         public InstantiableDomainException(string message) : base(message) { }
 
         public InstantiableDomainException(string message, Exception inner) : base(message, inner) { }
-
-        public InstantiableDomainException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 
     [Fact]
@@ -24,11 +22,9 @@ public class DomainExceptionTest
         static void ThrowFirstConstructor() => throw new InstantiableDomainException();
         static void ThrowSecondConstructor() => throw new InstantiableDomainException("Test message");
         static void ThrowThirdConstructor() => throw new InstantiableDomainException("Test message", new InstantiableDomainException());
-        static void ThrowFourthConstructor() => throw new InstantiableDomainException(null, new StreamingContext(StreamingContextStates.All));
 
         Assert.ThrowsAny<DomainException>(ThrowFirstConstructor);
         Assert.ThrowsAny<DomainException>(ThrowSecondConstructor);
         Assert.ThrowsAny<DomainException>(ThrowThirdConstructor);
-        Assert.ThrowsAny<DomainException>(ThrowFourthConstructor);
     }
 }
