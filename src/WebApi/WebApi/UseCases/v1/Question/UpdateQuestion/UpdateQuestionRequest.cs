32bf5876d68ac1ac4368bde164df752f26fa3bd0
ali kabbadj <KABBADJ.ALI@ofppt-edu.ma>
2022-04-11 17:25:13 +0000

    ## Summary Added the necessary validation attributes to the Dificulty property
    ## Summary
    
    Added [DisallowNull] , [Range] and [] atributes to the Difficulty properly  in :
    *CreateQuestionRequest
    *UpdateQuestionRequest
    
    Added a custum Validation Attribute to the Difficulties properly  in :
    *GetQuestionRequest
    
    Note: For Difficulties properly in GetQuesttionRequest , i added an extention to the ValidationAtribute Class to be able to validate the range of an IEnumerable<int>
    
    ## Types of changes
    - [x] Refactoring (non-breaking change to code and structure)
    
    ## Notes
    
    I was wondering if the custume validation for  IEnumerable<int> Difficulties should maybe return all the question for the difficulties that were correct and just ignore the out of range ones instead of what my implementation does which is return an error if one of the values in the IEnumerable is out oof range

--- src/WebApi/WebApi/UseCases/v1/Question/UpdateQuestion/UpdateQuestionRequest.cs
+++ src/WebApi/WebApi/UseCases/v1/Question/UpdateQuestion/UpdateQuestionRequest.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WebApi.UseCases.v1.Question.UpdateQuestion;
 
@@ -7,7 +8,7 @@ public record UpdateQuestionRequest
     [MaxLength(100)]
     public string Title { get; set; }
 
-    [Range(1, 5)]
+    [Range(0, 100, ErrorMessage = "The Difficulty field is out of range ( 0 - 100 )")]
     public int? Difficulty { get; set; }
 
     [MaxLength(50)]
