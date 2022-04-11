5affb0e09b550c5d7710d3ce2019a9103f822ee3
TerezaSkutova <TerezaSkutova@users.noreply.github.com>
2022-04-11 15:59:54 +0200

    Code cleanup
    The QuestionSetListItem included an unused using directives and namespace wasn'scoped.

--- src/WebApi/Domain/Models/Agreggates/QuestionSetListItem.cs
+++ src/WebApi/Domain/Models/Agreggates/QuestionSetListItem.cs
@@ -1,16 +1,11 @@
 ﻿using Domain.Models.ValueObject;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Domain.Models.Views
+namespace Domain.Models.Views;
+
+public class QuestionSetListItem
 {
-    public class QuestionSetListItem
-    {
-        public QuestionSetModel QuestionSet { get; init; }
-        public Difficulty Difficulty { get; set; }
-        public IEnumerable<Category> Tags { get; set; }
-    }
+    public QuestionSetModel QuestionSet { get; init; }
+    public Difficulty Difficulty { get; set; }
+    public IEnumerable<Category> Tags { get; set; }
 }
