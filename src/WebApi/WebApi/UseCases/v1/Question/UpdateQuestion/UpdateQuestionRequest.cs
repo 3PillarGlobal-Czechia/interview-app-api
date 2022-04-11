6f3f8497c3afc0c397084f6f0e3ab71efce0f6bc
ali kabbadj <KABBADJ.ALI@ofppt-edu.ma>
2022-04-11 19:48:29 +0000

    -Changed the value assigned to difficultly  in  UpdateQuestionTests to 150 To fix test failing in End2EndTests , due to changing range from (1-5) to (0-100)
    -Changed Range attribute (min,max) in UpdateQuestionRequest for Difficulty to the correct value (0-100)

--- src/WebApi/WebApi/UseCases/v1/Question/UpdateQuestion/UpdateQuestionRequest.cs
+++ src/WebApi/WebApi/UseCases/v1/Question/UpdateQuestion/UpdateQuestionRequest.cs
@@ -8,7 +8,7 @@ public record UpdateQuestionRequest
     [MaxLength(100)]
     public string Title { get; set; }
 
-    [Range(1, 5)]
+    [Range(0, 100, ErrorMessage = "The Difficulty field is out of range ( 0 - 100 )")]
     public int? Difficulty { get; set; }
 
     [MaxLength(50)]
